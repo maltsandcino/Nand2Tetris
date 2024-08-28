72de2fc2c4387176f42d22eb48bee73982d3a880
Tom <thomasjh@gmail.com>
2024-08-28 11:46:38 +0200

    fixing label counting

--- projects/6/symbols.py
+++ projects/6/symbols.py
@@ -6,40 +6,49 @@ symbol_table = {
 
 def convert_symbols(instructions: list) -> list:
 
-    instruction_number = 0
+    
+    code_sweep = 0
     first_available_slot = 16
 
     # Handling Labels
-    for i, line in enumerate(instructions):
-        if line[0] == "(":
-            temp_symbol = line[1:-1]
-            if temp_symbol not in symbol_table:
-                symbol_table[temp_symbol] = instruction_number
+    for sweep in range(2):
+        instruction_number = 0
 
+        for i, line in enumerate(instructions):
+            if line[0] == "(":
+                temp_symbol = line[1:-1]
+                if temp_symbol not in symbol_table:
+                    symbol_table[temp_symbol] = instruction_number
+                
+                instruction_number -= 1
 
-    # Handling either A Instructions (do nothing besides increment the instructions) or variable declaration/use
-        if line[0] == '@':
-            temp_symbol = line[1:]
-            if temp_symbol.isdigit():
+            if code_sweep > 0:
+        # Handling either A Instructions (do nothing besides increment the instructions) or variable declaration/use
+                if line[0] == '@':
+                    temp_symbol = line[1:]
+                    if temp_symbol.isdigit():
+                        instruction_number += 1
+                        continue
+
+            # If present, just grab the value from the table
+                    if temp_symbol in symbol_table:
+                        instructions[i] = f"@{symbol_table[temp_symbol]}"
+            
+            # If not present in table, find the first available RAM slot we can use by looking at the values in the table, and assign that to the NEW symbol.
+                    if temp_symbol not in symbol_table:
+                        while first_available_slot in symbol_table.values():
+                            first_available_slot += 1
+                        symbol_table[temp_symbol] = first_available_slot
+                        instructions[i] = f"@{first_available_slot}"
+
+            # Increment Instructions (We need do do this to keep track of labels)
+                    instruction_number += 1
+                
+                else:
+                    instruction_number += 1
+            else:
                 instruction_number += 1
-                continue
-
-    # If present, just grab the value from the table
-            if temp_symbol in symbol_table:
-                instructions[i] = f"@{symbol_table[temp_symbol]}"
-    
-    # If not present in table, find the first available RAM slot we can use by looking at the values in the table, and assign that to the NEW symbol.
-            if temp_symbol not in symbol_table:
-                while first_available_slot in symbol_table.values():
-                    first_available_slot += 1
-                symbol_table[temp_symbol] = first_available_slot
-                instructions[i] = f"@{first_available_slot}"
-
-    # Increment Instructions (We need do do this to keep track of labels)
-            instruction_number += 1
-        
-        else:
-            instruction_number += 1
+        code_sweep += 1
 
     instructions = [line for line in instructions if line[0] != '(']
 
